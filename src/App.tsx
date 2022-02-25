332e91edd93b4dc393d79172ad28f05ab235b90c
Manuel Luna <manuellunada@gmail.com>
2022-02-25 16:10:22 -0600

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,26 @@
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
 
 const Home = () => {
-  return <Typography variant="h1">Hello</Typography>;
+  return (
+    <div className={styles.container}>
+      <Typography variant="h1" align="center" color="white">
+        Welcome to Lily ✨
+      </Typography>
+
+      <div className={styles.buttons}>
+        <div>
+					<Button variant="contained" color="info">
+						<Link to="/login">Log in</Link>
+					</Button>
+          <Button variant="contained" color="secondary">Sign in</Button>
+        </div>
+					<Button variant="contained" color="error">Go to app</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
